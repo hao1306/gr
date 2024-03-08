41acae573ca49073bfe0c0d01de16b69ba0f94a3
Hao Yang <93369114+hao1306@users.noreply.github.com>
2024-03-08 18:18:22 +0100

    latest version

--- network/rnn.py
+++ network/rnn.py
@@ -34,26 +34,25 @@ class MyNetwork(nn.Module):
     def __init__(self):
         super(MyNetwork, self).__init__()
         self.resnet = ResNet50Custom(num_classes=1000)
-        self.lstm = CustomLSTM(input_size=12, hidden_size=48, num_layers=2, num_classes=12)
-        self.fc01 = nn.Linear(2, 1000)
+        # self.lstm = CustomLSTM(input_size=12, hidden_size=48, num_layers=2, num_classes=12)
+        self.fc01 = nn.Linear(3, 1000)
+        self.fc03 = nn.Linear(72, 18)
         self.fc02 = nn.Sequential(
             nn.Linear(2000, 1000),
             nn.ReLU(),
-            nn.Linear(1000,500),
+            nn.Linear(1000,100),
             nn.ReLU(),
-            nn.BatchNorm1d(500),
-            nn.Linear(500,100),
-            nn.Linear(100, 12),
-            nn.BatchNorm1d(12)
+            nn.BatchNorm1d(100),
+            nn.Linear(100, 18)
         )
 
     def forward(self, x_img, x_coordinate):
         size_img = x_img.size()  # batch * 4 * image
-        size_coo = x_coordinate.size()  # batch * 4 * 2
+        size_coo = x_coordinate.size()  # batch * 4 * 3
 
         #  merge the first and second dimension to avoid recurring function
         img_new = x_img.reshape(size_img[0] * size_img[1], *size_img[2:])  # 4*batch * image
-        coo_new = x_coordinate.reshape(size_coo[0] * size_coo[1], *size_coo[2:])  # 4*batch * 2
+        coo_new = x_coordinate.reshape(size_coo[0] * size_coo[1], *size_coo[2:])  # 4*batch * 3
 
         # process images and coordinates
         x1 = self.resnet(img_new)  # 4*batch * 1000
@@ -63,17 +62,121 @@ class MyNetwork(nn.Module):
         # print('shape of x2')
         # print(x2.size())
         x3 = torch.cat((x1, x2), dim=1)  # concatinate x1 and x2 to get 4*batch * 2000
-        x3 = self.fc02(x3)  # 4*batch * 12
+        x3 = self.fc02(x3)  # 4*batch * 18
         num_node = x3.size(1)
-        x4 = x3.view(x_coordinate.size(0), x_coordinate.size(1), num_node)  # batch * 4 * 12
+        x4 = x3.view(x_coordinate.size(0), x_coordinate.size(1), num_node)  # batch * 4 * 18
+
+        x4 = x4.flatten(1, 2)
+        # print('shape of x4')
+        # print(x4.size())
+
+        # 输入到LSTM网络
+        # lstm_out = self.lstm(x4)
+
+        # print('shape of lstm out', lstm_out.size())  # batchsize * 12
+
+        # output = lstm_out.reshape(-1, 6, 2)
+        output = self.fc03(x4)
+        output = output.reshape(-1, 6, 3)
+
+        # print('shape of output', output.size())  # batch size * 6 * 2
+
+        return output
+
+class MyNetwork_LSTM(nn.Module):
+    def __init__(self):
+        super(MyNetwork_LSTM, self).__init__()
+        self.resnet = ResNet50Custom(num_classes=1000)
+        self.lstm = CustomLSTM(input_size=18, hidden_size=48, num_layers=2, num_classes=18)
+        self.fc01 = nn.Linear(3, 1000)
+        self.fc02 = nn.Sequential(
+            nn.Linear(2000, 1000),
+            nn.ReLU(),
+            nn.Linear(1000,100),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Linear(100, 18)
+        )
+
+    def forward(self, x_img, x_coordinate):
+        size_img = x_img.size()  # batch * 4 * image
+        size_coo = x_coordinate.size()  # batch * 4 * 3
+
+        #  merge the first and second dimension to avoid recurring function
+        img_new = x_img.reshape(size_img[0] * size_img[1], *size_img[2:])  # 4*batch * image
+        coo_new = x_coordinate.reshape(size_coo[0] * size_coo[1], *size_coo[2:])  # 4*batch * 3
+
+        # process images and coordinates
+        x1 = self.resnet(img_new)  # 4*batch * 1000
+        # print('shape of x1')
+        # print(x1.size())
+        x2 = self.fc01(coo_new)  # 4*batch * 1000
+        # print('shape of x2')
+        # print(x2.size())
+        x3 = torch.cat((x1, x2), dim=1)  # concatinate x1 and x2 to get 4*batch * 2000
+        x3 = self.fc02(x3)  # 4*batch * 18
+        num_node = x3.size(1)
+        x4 = x3.view(x_coordinate.size(0), x_coordinate.size(1), num_node)  # batch * 4 * 18
 
         # 输入到LSTM网络
         lstm_out = self.lstm(x4)
 
         # print('shape of lstm out', lstm_out.size())  # batchsize * 12
 
-        output = lstm_out.reshape(-1, 6, 2)
+        output = lstm_out.reshape(-1, 6, 3)
 
         # print('shape of output', output.size())  # batch size * 6 * 2
+
+        return output
+
+class MyNetwork_NOimage(nn.Module):
+    def __init__(self):
+        super(MyNetwork_NOimage, self).__init__()
+        # self.resnet = ResNet50Custom(num_classes=1000)
+        # self.lstm = CustomLSTM(input_size=12, hidden_size=48, num_layers=2, num_classes=12)
+        self.fc01 = nn.Linear(12, 1000)
+        self.fc03 = nn.Linear(72, 18)
+        self.fc02 = nn.Sequential(
+            nn.Linear(1000, 500),
+            nn.ReLU(),
+            nn.Linear(500,100),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Linear(100, 18)
+        )
+
+    def forward(self, x_coordinate):
+        coo_new = x_coordinate.flatten(1, 2)
+        # print(coo_new.size())
+        x1 = self.fc01(coo_new)
+        x1 = self.fc02(x1)
+        output = x1.reshape(-1, 6, 3)
+
+        return output
+    
+class MyNetwork_LSTM_NOimage(nn.Module):
+    def __init__(self):
+        super(MyNetwork_LSTM_NOimage, self).__init__()
+        # self.resnet = ResNet50Custom(num_classes=1000)
+        self.lstm = CustomLSTM(input_size=18, hidden_size=48, num_layers=2, num_classes=18)
+        self.fc01 = nn.Linear(12, 1000)
+        # self.fc03 = nn.Linear(72, 18)
+        self.fc02 = nn.Sequential(
+            nn.Linear(1000, 500),
+            nn.ReLU(),
+            nn.Linear(500,100),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Linear(100, 72)
+        )
+
+    def forward(self, x_coordinate):
+        x_size = x_coordinate.size()
+        coo_new = x_coordinate.flatten(1, 2)
+        x1 = self.fc01(coo_new)
+        x1 = self.fc02(x1)
+        x1 = x1.reshape(x_size[0], 4, 18)
+        output = self.lstm(x1)
+        output = output.reshape(-1, 6, 3)
 
         return output
